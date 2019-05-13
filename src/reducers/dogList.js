171ec24a731cde37f381fcf3075c6968e513f9a7
Alena <elen.malets@gmail.com>
2019-05-13 17:05:15 +0200

    fetch data in dogs list

--- src/reducers/dogList.js
+++ src/reducers/dogList.js
@@ -3,7 +3,7 @@ import { SET_DOGS } from '../actions/getDogs'
 const reducer = (state = [], action = {}) => {
     switch (action.type) {
         case SET_DOGS:  
-            return action.payload.map(dogs => ({ ...dogs }))
+            return {...action.payload}
         default:
             return state
     }
