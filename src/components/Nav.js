ba5eb041233a6b861204e5acbd08ee9d42ee15e7
Samaneh Dallalizad <samaneh.dallalizad@gmail.com>
2019-05-13 15:06:24 +0200

    nav compomnent

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+
+export default class Nav extends Component{
+
+    render(){      
+         
+        
+
+        return (
+            <div>
+                <span><a href="/">dogLists</a></span> 
+                
+                <span>game1</span>                
+            </div>
+        );
+    }
+
+}
