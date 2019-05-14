953ffae0f966919daa7e8efdd9e9644823376e4f
Samaneh Dallalizad <samaneh.dallalizad@gmail.com>
2019-05-14 10:56:52 +0200

    shuffle-answer

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -2,17 +2,51 @@ import React, { Component } from 'react'
 import {getRandomImage} from '../actions/getRandomImage'
 import { connect } from 'react-redux';
 
+
+const shuffle = function (array) {
+
+	let currentIndex = array.length;
+	let temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+
+};
+const answers = ['option1', 'option2', 'option3'];
+
 class Game1 extends Component {
 
-  componentDidMount(){
-    this.props.getRandomImage();
+componentDidMount(){
+   
+   shuffle(answers);
+    this.props.getRandomImage();  
+ 
 }
   render() {
     return (
       <div>
         <h1>I'm a game 1</h1>
-        <img src={this.props.randomImage}></img>
+        <img src={this.props.randomImage}></img>     
+        <p>please choose correct answer:</p>
+    {answers.map((answer,index )=> <div className="radio" key={index} ><input type="radio" value={answer}  />{answer}</div>)}
+
+
       </div>
+       
+      
+
+   
     )
   }
 }
