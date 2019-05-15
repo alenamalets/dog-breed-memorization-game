5ba771845efc7687b597541bac2ad2710612e111
davemollen <davemollen@gmail.com>
2019-05-15 15:13:55 +0200

    fixed-game2-actions

--- src/actions/gameTwoActions.js
+++ src/actions/gameTwoActions.js
@@ -8,34 +8,41 @@ export function getCorrectName (dogList){
     return correctName
 }
 
-export function getUrlArray(arrayofnames) {
-    const newArray = arrayofnames.map(name => {
-        return `https://dog.ceo/api/breed/${name}/images`
-    })
-    return newArray
-}
-
-export function setupQuestion(dogsList){
-    const correctAnswer = getCorrectName(dogsList);
-    const allAnswers = answersNoRepeat(dogsList, correctAnswer);
-    const allImages = getUrlArray(allAnswers)
-    return {
-      type: GAME_2,
-      payload: {
-        dogs: [...dogsList],
-        correctAnswer: correctAnswer,
-        answers: allAnswers,
-        images: allImages
-      }
-    }
+const pickRandomImgUrl = (images) => {
+  const randomNumber = Math.floor(Math.random() * images.length);
+  return images[randomNumber];
 }
 
 export function getRandomImage(){
-    return function (dispatch, getState){  
-      request('https://dog.ceo/api/breeds/image/random')
-        .then(response => {
-          const dogsList = getState().dogsList;
-          dispatch(setupQuestion(dogsList))
-        })
+  return function (dispatch, getState){  
+    const dogsList = getState().dogsList;
+    const correctAnswer = getCorrectName(dogsList);
+    const allAnswers = answersNoRepeat(dogsList, correctAnswer);
+    const allImages = [];
+    request(`https://dog.ceo/api/breed/${allAnswers[0]}/images`)
+      .then(response => {
+        allImages.push(pickRandomImgUrl(response.body.message));
+      })
+    request(`https://dog.ceo/api/breed/${allAnswers[1]}/images`)
+      .then(response => {
+        allImages.push(pickRandomImgUrl(response.body.message));
+      })
+    request(`https://dog.ceo/api/breed/${allAnswers[2]}/images`)
+      .then(response => {
+        allImages.push(pickRandomImgUrl(response.body.message));
+        dispatch(setupQuestion(dogsList, correctAnswer, allAnswers, allImages))
+      })
+  }
+}
+
+export function setupQuestion(dogsList, correctAnswer, allAnswers, allImages){
+  return {
+    type: GAME_2,
+    payload: {
+      dogs: [...dogsList],
+      correctAnswer: correctAnswer,
+      answers: allAnswers,
+      images: allImages
     }
   }
+}
