9e736983711e62fb2ce0ce6275275cff13773759
davemollen <davemollen@gmail.com>
2019-05-15 12:07:27 +0200

    some indenting

--- src/actions/getDogs.js
+++ src/actions/getDogs.js
@@ -2,19 +2,19 @@ import * as request from 'superagent'
 export const SET_DOGS = 'SET_DOGS'
 
 export function setDogList(dogs) {
-    return {
-      type: SET_DOGS,
-      payload: dogs
-    }
+  return {
+    type: SET_DOGS,
+    payload: dogs
   }
+}
   
-  export function getDogs() {
-    return function (dispatch) {  
-      request('https://dog.ceo/api/breeds/list/all')
-        .then(response => {
-         // console.log(response.body.message);
-          
-          dispatch(setDogList(response.body.message))
-        })
-    }
+export function getDogs() {
+  return function (dispatch) {  
+    request('https://dog.ceo/api/breeds/list/all')
+      .then(response => {
+        // console.log(response.body.message);
+        
+        dispatch(setDogList(response.body.message))
+      })
   }
+}
